2d699bfaeed31fa1b3833a77c92ca4bc4f2aed65
Matt Holmes <holmesm8@gmail.com>
2020-03-04 10:25:33 -0700

    Fixed issue with discount percent

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,9 +1,9 @@
 class Discount <ApplicationRecord
   belongs_to :merchant
 
-  validates_presence_of :name,
-                        :percent_off,
-                        :min_quantity
+  validates_presence_of :name
+  validates_presence_of :percent_off, less_than_or_equal_to: 100
+  validates_presence_of :min_quantity
 
   has_many :item_discounts, dependent: :destroy
   has_many :items, through: :item_discounts
