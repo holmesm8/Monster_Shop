a1f0752fa406d2fdab6da9850afb18b43f5073d4
Matt Holmes <holmesm8@gmail.com>
2020-03-04 12:52:39 -0700

    Fix bug to not allow negative numbers in discount

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,8 +2,11 @@ class Discount <ApplicationRecord
   belongs_to :merchant
 
   validates_presence_of :name
-  validates_presence_of :percent_off, less_than_or_equal_to: 100
-  validates_presence_of :min_quantity
+  # validates_presence_of :percent_off, less_than_or_equal_to: 100
+  # validates_presence_of :min_quantity, more_than: 0
+
+  validates :percent_off, presence: true, numericality: { greater_than: 0, less_than: 100 }
+  validates :min_quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   has_many :item_discounts, dependent: :destroy
   has_many :items, through: :item_discounts
